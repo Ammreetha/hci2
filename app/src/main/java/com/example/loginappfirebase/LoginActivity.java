b3e4c69bf3e3ef5498ac73f54a8a7f7ff6721b5d
Pavapratha <pavapratha414@gmail.com>
2024-11-27 21:33:24 +0530

    initial commit

--- app/src/main/java/com/example/loginappfirebase/LoginActivity.java
+++ app/src/main/java/com/example/loginappfirebase/LoginActivity.java
@@ -9,12 +9,8 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -52,7 +48,7 @@ public class LoginActivity extends AppCompatActivity {
                                     @Override
                                     public void onSuccess(AuthResult authResult) {
                                         Toast.makeText(LoginActivity.this,"Login Successful",Toast.LENGTH_SHORT).show();
-                                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                        startActivity(new Intent(LoginActivity.this, registrationActivity.class));
                                         finish();
                                     }
                                 }).addOnFailureListener(new OnFailureListener() {
